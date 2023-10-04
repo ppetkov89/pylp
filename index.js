ae70a10e6138a4dfd8da2de6237fbac4e4a29123
Petko Petkov <petko.petkov@ffwagency.com>
2023-10-04 16:16:38 +0300

    remove taker

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const taker = require('./src/tasks/taker');
 var vfs = require('vinyl-fs');
 
 module.exports = {
-  taker: taker,
   task: taker.task.bind(taker),
   src: vfs.src.bind(vfs),
   dest: vfs.dest.bind(vfs),
